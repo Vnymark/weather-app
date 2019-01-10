d9076304414ef302bfbd32cdbe82e21c50a3cec6
Viktor Nymark <vnymark@gmail.com>
2019-01-10 22:07:41 +0100

    Added Guzzle as a HTTP client and fixed a GET request (using Guzzle) that only supports response 200 for now.

--- index.php
+++ index.php
@@ -1,12 +1,27 @@
 <?php require 'vendor/autoload.php';
 
-// Instantiate the App object
-$app = new \Slim\App();
+// Create a new Container with Guzzle injected
+$container = new \Slim\Container([
+    'http' => function () {
+        return new GuzzleHttp\Client();
+    }
+]);
 
-// Declare routes
+// Instantiate the App object
+$app = new \Slim\App($container);
+
+// Get weather by id (890869 for Gothenburg - http://woeid.rosselliot.co.nz)
 $app->get('/locations/{id}', function ($request, $response, $args) {
-    return $response->withStatus(200)->write("Location {$args['id']} retrieved.");
+    // Get the weather from MetaWeather
+    $result =
+        $this->http->get("https://www.metaweather.com/api/location/{$args['id']}")
+            ->getBody()
+            ->getContents();
+    // Return the result as JSON
+    return $response->withStatus(200)->withJson(json_decode($result));
 });
+
+// Delete weather by id
 $app->delete('/locations/{id}', function ($request, $response, $args) {
     return $response->withStatus(200)->write("Location {$args['id']} deleted.");
 });
